cc873be6ca2bad3b04f63f74458510ed222ab66c
rakaso598 <nexonaddress98@gmail.com>
2025-02-25 17:17:21 +0900

    feat: Added Repo Link

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
     <div className="container">
       <span className="title">Which is your favorite color?</span>
       <Choice />
+      <a className="bio" href="https://github.com/rakaso598/color-button">
+        Visit My Git Repo
+      </a>
     </div>
   );
 }
